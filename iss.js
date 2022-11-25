5d9b527d01eba47d2ec4ebc8e986da00f121dad1
Likai Liu <likailiu2@gmail.com>
2022-11-25 14:47:17 +0000

    Implement a geo coordinates fetcher function

--- iss.js
+++ iss.js
@@ -15,4 +15,32 @@ const fetchMyIp = (callback) => {
   });
 };
 
-module.exports = { fetchMyIp };
+const fetchCoordsByIp = (ip, callback) => {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    // parse the returned body for convenient use
+    const geoInfo = JSON.parse(body);
+    // handling invalid ip by checking the success status in the returned body
+    if (!geoInfo.success) {
+      const msg = `Success status: ${geoInfo.success}. Server message says: ${geoInfo.message} when fetching for IP ${geoInfo.ip}`;
+      callback(Error(msg), null);
+      return;
+    }
+    // extract coordinates from parsed body to a new object
+    // const geoCoords = {};
+    // geoCoords.latitude = geoInfo.latitude;
+    // geoCoords.longitude = geoInfo.longitude;
+    // use object shorthand syntax:
+    const { latitude, longitude } = geoInfo; 
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = {
+  fetchMyIp,
+  fetchCoordsByIp
+};
